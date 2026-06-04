19fb1cf63d475a58b3e9438b6564e0657d0ba79e
seminof1996-ops <seminof1996@gmail.com>
2026-06-04 02:57:44 -0700

    my first commit

---
+++ test.py
@@ -0,0 +1,25 @@
+
+"""
+var1 = "informationtechnology"
+print(var1[1:8:2])
+print(var1[1::3])"""
+""""
+my_fruits = ["orange", "apple", "mango", "banana", "grape", "cucumber", "papaya", "watermelon"]
+print(my_fruits[4])
+sweetest = my_fruits[4]
+print(sweetest[0]) """
+
+
+"""
+my_guy = "  obi is a boy  "
+print(my_guy.upper())
+print(my_guy.capitalize())
+print(my_guy.split())
+print(my_guy.strip()) 
+
+#list2
+var1 = "obi is a boy"
+var2 = "ada is a girl"
+print(var1 + " " + var2)
+print(var1 + " " + var2)
+"""
