7518ae595625fd59512764ee9bad886d7f8a220b
seminof1996-ops <seminof1996@gmail.com>
2026-06-08 11:18:10 -0700

    modified

--- king12.py
+++ king12.py
@@ -1,1 +1,14 @@
-g
+# # #   print(bool("00"))
+# # #   print(5==7)
+#   print(5!=7)
+# # # # a = 10 
+# # # # a %= 3
+# # # # print (a)
+
+# # x = y = 5
+# # print(y)
+# # print("x=y")
+# # list1 = ("dog" "cat" "goat")
+# # print("dog" in list1)
+list_fruit = ["banana" "orange" "apple"]
+print(type(list_fruit))
